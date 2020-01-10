ff686029b055be83a177edbcb66a5d13c13751a2
Notelzg <740207611@qq.com>
2020-01-10 22:19:06 +0800

    添加一个线程安全的消息队列

--- interview/test/ObjectCache.java
+++ interview/test/ObjectCache.java
@@ -1,13 +1,33 @@
 package test;
-public class ObjectCache {
+
+import java.lang.management.ManagementFactory;
+
+public class ObjectCache{
+    static class t extends Thread{
+        public void run(){
+            while (true) {
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                printMsg();
+            }
+        }
+    }
+    public static  void printMsg() {
+        Thread t = Thread.currentThread();
+        String name = t.getName();
+        System.out.println("name=" + name+ " pid=" + t.getId() );
+        // get name representing the running Java virtual machine.
+        name = ManagementFactory.getRuntimeMXBean().getName();
+        System.out.println(name);
+        String pid = name.split("@")[0];
+        System.out.println("Pid is:" + pid);
+    }
     public static void  main(String[] args){
-        String str = new String("abc");
-        String str2 = new String("abc");
-        System.out.println(str2 == str);
-        String str3 = "abc";
-        String str4 = "abc";
-        System.out.println(str3 == str4);
-
-
+        t tt = new t();
+        tt.start();
+        printMsg();
     }
 }
